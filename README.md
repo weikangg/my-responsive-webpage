426e1f636ef091685090ec28ff7e311a025b1985
Wei <chongweikang5@gmail.com>
2022-06-10 21:14:53 +0800

    completed the layout of the website

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # my-responsive-webpage
+
+- Boxicons websites has cool icons to use from
+- Importing google fonts
+- Dark & Light Mode Implementation
+- Responsiveness of website using media queries
+- Sending emails with buttons
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// When the icon is clicked, the class name of the button becomes is-black
+document.querySelector(".header_moon").onclick = function(){
+    document.querySelector("body").classList.toggle("is-black")
+}
+
+// When the screen becomes tablet size, the mob icon comes out 
+document.querySelector(".header__mob-menu").onclick = function(){
+    document.querySelector(".header-menu").classList.toggle("is-active")
+}
